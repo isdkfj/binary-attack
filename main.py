8bb86d4ba4d15a10fd779404f6e10d48004a22c0
叶芃 <isdkfj@qq.com>
2022-06-20 09:40:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,10 +23,11 @@ def run_exp(D1, num_exp, mask):
         sum_train_acc += train_acc
         sum_test_acc += test_acc
         sum_attack_acc += attack_acc
+        print(train_acc, test_acc, attack_acc)
     mask.print_info(sum_train_acc / num_exp, sum_test_acc / num_exp, sum_attack_acc / num_exp)
 
 for D1 in [8]:
-    gauss = Gaussian(0.0)
+    '''gauss = Gaussian(0.0)
     run_exp(D1, 10, gauss)
     gauss = Gaussian(0.01)
     run_exp(D1, 10, gauss)
@@ -35,7 +36,7 @@ for D1 in [8]:
     gauss = Gaussian(0.1)
     run_exp(D1, 10, gauss)
     gauss = Gaussian(0.5)
-    run_exp(D1, 10, gauss)
+    run_exp(D1, 10, gauss)'''
     fab = Defense(D1)
     run_exp(D1, 10, fab)
         
