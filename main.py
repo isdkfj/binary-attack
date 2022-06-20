d6f4f60e828cde4a7aa47f8e1937d3a0be3818e8
叶芃 <isdkfj@qq.com>
2022-06-20 19:25:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ num_classes = 2
 if args.data == 'nursery':
     num_classes = 5
 
-def run_exp(D1, num_exp, mask):
+def run_exp(d1, num_exp, mask):
     sum_train_acc = 0
     sum_test_acc = 0
     sum_attack_acc = 0
     for iter_exp in range(num_exp):
-        net = Net(D1, train_X.shape[1] - D1 - 1, num_classes, args.net, mask.defense)
+        net = Net(d1, train_X.shape[1] - d1 - 1, num_classes, args.net, mask.defense)
         train(net, (train_dataset, train_loader, test_dataset, test_loader))
         train_acc, test_acc, attack_acc, idx = eval(net, (train_dataset, train_loader, test_dataset, test_loader))
         sum_train_acc += train_acc
@@ -32,18 +32,18 @@ def run_exp(D1, num_exp, mask):
 dimensions = [8]
 if args.data == 'nursery':
     dimensions = [6]
-for D1 in dimensions:
-    gauss = Gaussian(0.0)
-    run_exp(D1, 10, gauss)
+for d1 in dimensions:
+    '''gauss = Gaussian(0.0)
+    run_exp(d1, 10, gauss)
     gauss = Gaussian(0.01)
-    run_exp(D1, 10, gauss)
+    run_exp(d1, 10, gauss)
     gauss = Gaussian(0.05)
-    run_exp(D1, 10, gauss)
+    run_exp(d1, 10, gauss)
     gauss = Gaussian(0.1)
-    run_exp(D1, 10, gauss)
+    run_exp(d1, 10, gauss)
     gauss = Gaussian(0.5)
-    run_exp(D1, 10, gauss)
-    fab = Defense(D1)
-    run_exp(D1, 10, fab)
+    run_exp(d1, 10, gauss)'''
+    fab = Defense(d1)
+    run_exp(d1, 10, fab)
         
         
