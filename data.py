196f9d4d30d188aa12f419420fabec05f6569b2d
叶芃 <isdkfj@qq.com>
2022-06-20 09:54:48 +0800

    Update data.py

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
-def load_data(dname, path):
+def load_data(dname, path, SEED):
     if dname == 'bank':
         path = os.path.join(path, 'bank-additional-full.csv')
         df = pd.read_csv(path, delimiter=';')
@@ -26,5 +26,5 @@ def load_data(dname, path):
         Y[:X_0.shape[0]] = 0
         fake_label = np.random.randint(0, 2, (X.shape[0], 1))
         X = np.concatenate([X, fake_label], axis=1)
-        train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.1, random_state=0)
+        train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.1, random_state=SEED)
     return train_X, test_X, train_Y, test_Y
