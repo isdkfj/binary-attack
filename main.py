512ec652595ef0881e367508513756587a5d53e8
叶芃 <isdkfj@qq.com>
2022-06-20 19:28:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ def run_exp(d1, num_exp, mask):
     sum_test_acc = 0
     sum_attack_acc = 0
     for iter_exp in range(num_exp):
-        net = Net(d1, train_X.shape[1] - d1 - 1, num_classes, args.net, mask.defense, verbose=True)
-        train(net, (train_dataset, train_loader, test_dataset, test_loader))
+        net = Net(d1, train_X.shape[1] - d1 - 1, num_classes, args.net, mask.defense)
+        train(net, (train_dataset, train_loader, test_dataset, test_loader), verbose=True)
         train_acc, test_acc, attack_acc, idx = eval(net, (train_dataset, train_loader, test_dataset, test_loader))
         sum_train_acc += train_acc
         sum_test_acc += test_acc
