15b15621f7a2ed44ea295c234b0fd9e4c502287c
叶芃 <isdkfj@qq.com>
2022-06-20 09:39:38 +0800

    Update defend.py

--- defend.py
+++ defend.py
@@ -22,7 +22,7 @@ class Defense:
         invW = torch.linalg.inv(W[:self.d1, :].T)
         Q = torch.linalg.solve(W[:self.d1, :].T, W.T)
         r = torch.zeros(x1.size())
-        w = torch.mean(invW, axis=1)
+        w = torch.mean(invW, axis=1).reshape(-1, 1)
         # construct quadratic programming
         mat = torch.zeros((self.d1 + 1, self.d1 + 1))
         mat[:self.d1, :self.d1] = 2 * Q @ Q.T
@@ -31,7 +31,7 @@ class Defense:
         vec = torch.zeros(self.d1 + 1)
         vec[-1] = 1
         sol = torch.linalg.solve(mat, vec)
-        r = x[:, -1].reshape(-1, 1) - x1[:, :self.d1] @ w.reshape(-1, 1)
+        r = x[:, -1].reshape(-1, 1) - x1[:, :self.d1] @ w
         r = r @ sol[:self.d1].reshape(1, -1)
         r = r[:, :self.d1] @ Q
         return r
