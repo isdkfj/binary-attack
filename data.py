96630155900482f23ee182745b5b25369799796d
叶芃 <isdkfj@qq.com>
2022-06-20 15:20:16 +0800

    Update data.py

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 
 def load_data(dname, path, SEED):
     if dname == 'bank':
-        path = os.path.join(path, 'bank-additional-full.csv')
+        path = os.path.join(path, '/bank-marketing/bank-additional-full.csv')
         df = pd.read_csv(path, delimiter=';')
         for attr in df.columns:
             if df[attr].dtype == 'object':
@@ -27,7 +27,7 @@ def load_data(dname, path, SEED):
         X = np.concatenate([X, fake_label], axis=1)
         train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.1, random_state=SEED)
     elif dname == 'credit':
-        path = os.path.join(path, 'UCI_Credit_Card.csv')
+        path = os.path.join(path, '/default-of-credit-card-clients-dataset/UCI_Credit_Card.csv')
         df = pd.read_csv(path, delimiter=',')
         for attr in df.columns:
             if df[attr].dtype == 'object':
@@ -45,6 +45,23 @@ def load_data(dname, path, SEED):
         X = np.concatenate((X_0, X_1), axis=0)
         Y = np.ones(X.shape[0], dtype=int)
         Y[:X_0.shape[0]] = 0
+        fake_label = np.random.randint(0, 2, (X.shape[0], 1))
+        X = np.concatenate([X, fake_label], axis=1)
+        train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.1, random_state=SEED)
+    elif dname == 'news':
+        pass
+    elif dname == 'mushroom':
+        path = os.path.join(path, '/mushroom-classification/mushrooms.csv')
+        df = pd.read_csv(path)
+        df = df.drop(columns=['veil-type'])
+        for attr in df.columns:
+            if df[attr].dtype == 'object':
+                encoder= LabelEncoder().fit(df[attr])
+                df[attr] = encoder.transform(df[attr])
+        X = df.values[:, 1:].astype('float')
+        X -= np.min(X, axis=0)
+        X /= np.max(X, axis=0)
+        Y = df.values[:, 0].astype('int')
         fake_label = np.random.randint(0, 2, (X.shape[0], 1))
         X = np.concatenate([X, fake_label], axis=1)
         train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.1, random_state=SEED)
