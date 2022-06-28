07ecacf2fa82766587e3e6cddbdd3796ceb9cf0c
叶芃 <isdkfj@qq.com>
2022-06-28 21:25:28 +0800

    Update data.py

--- data.py
+++ data.py
@@ -17,12 +17,12 @@ def load_data(dname, path, SEED):
         X /= np.max(X, axis=0)
         Y = df.values[:, -1].astype('int')
         # balance
-        X_0 = X[Y == 0]
+        '''X_0 = X[Y == 0]
         X_1 = X[Y == 1]
         X_0 = X_0[np.random.choice(X_0.shape[0], X_1.shape[0])]
         X = np.concatenate((X_0, X_1), axis=0)
         Y = np.ones(X.shape[0], dtype=int)
-        Y[:X_0.shape[0]] = 0
+        Y[:X_0.shape[0]] = 0'''
         fake_label = np.random.randint(0, 2, (X.shape[0], 1))
         X = np.concatenate([X, fake_label], axis=1)
         train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.1, random_state=SEED)
