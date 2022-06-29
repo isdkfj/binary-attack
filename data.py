471dffcb2dfe694ca5f8668f152ad910ea4f9f9f
叶芃 <isdkfj@qq.com>
2022-06-29 14:06:53 +0800

    Update data.py

--- data.py
+++ data.py
@@ -4,6 +4,19 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
+def process_binary(X):
+    print('#instances: {}, #features: {}'.format(X.shape[0], X.shape[1]))
+    for i in range(X.shape[1]):
+        s0 = np.sum(np.isclose(X[:, i], 0))
+        s1 = np.sum(np.isclose(X[:, i], 1))
+        if s0 + s1 == X.shape[0]:
+            if s0 < s1:
+                # swap 0 and 1 if there are more 1's
+                X[:, i] = 1 - X[:, i]
+                s0, s1 = s1, s0
+            print('feature no.{} is binary, {}% are 0\'s'.format(i, s0 / X.shape[0] * 100))
+            print(np.sum(np.isclose(X[:, i], 0)) / X.shape[0])
+
 def load_data(dname, path, SEED):
     if dname == 'bank':
         path = os.path.join(path, 'bank-marketing/bank-additional-full.csv')
@@ -13,16 +26,8 @@ def load_data(dname, path, SEED):
                 encoder= LabelEncoder().fit(df[attr])
                 df[attr] = encoder.transform(df[attr])
         X = df.values[:, :-1]
-        X -= np.min(X, axis=0)
-        X /= np.max(X, axis=0)
         Y = df.values[:, -1].astype('int')
-        # balance
-        '''X_0 = X[Y == 0]
-        X_1 = X[Y == 1]
-        X_0 = X_0[np.random.choice(X_0.shape[0], X_1.shape[0])]
-        X = np.concatenate((X_0, X_1), axis=0)
-        Y = np.ones(X.shape[0], dtype=int)
-        Y[:X_0.shape[0]] = 0'''
+        process_binary(X)
         fake_label = np.random.randint(0, 2, (X.shape[0], 1))
         X = np.concatenate([X, fake_label], axis=1)
         train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.1, random_state=SEED)
@@ -35,16 +40,8 @@ def load_data(dname, path, SEED):
                 df[attr] = encoder.transform(df[attr])
         df = df.drop(columns=['ID'])
         X = df.values[:, :-1]
-        X -= np.min(X, axis=0)
-        X /= np.max(X, axis=0)
         Y = df.values[:, -1].astype('int')
-        # balance
-        '''X_0 = X[Y == 0]
-        X_1 = X[Y == 1]
-        X_0 = X_0[np.random.choice(X_0.shape[0], X_1.shape[0])]
-        X = np.concatenate((X_0, X_1), axis=0)
-        Y = np.ones(X.shape[0], dtype=int)
-        Y[:X_0.shape[0]] = 0'''
+        process_binary(X)
         fake_label = np.random.randint(0, 2, (X.shape[0], 1))
         X = np.concatenate([X, fake_label], axis=1)
         train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.1, random_state=SEED)
@@ -59,9 +56,8 @@ def load_data(dname, path, SEED):
                 encoder= LabelEncoder().fit(df[attr])
                 df[attr] = encoder.transform(df[attr])
         X = df.values[:, 1:].astype('float')
-        X -= np.min(X, axis=0)
-        X /= np.max(X, axis=0)
         Y = df.values[:, 0].astype('int')
+        process_binary(X)
         fake_label = np.random.randint(0, 2, (X.shape[0], 1))
         X = np.concatenate([X, fake_label], axis=1)
         train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.1, random_state=SEED)
@@ -73,10 +69,15 @@ def load_data(dname, path, SEED):
                 encoder= LabelEncoder().fit(df[attr])
                 df[attr] = encoder.transform(df[attr])
         X = df.values[:, :-1].astype('float')
-        X -= np.min(X, axis=0)
-        X /= np.max(X, axis=0)
         Y = df.values[:, -1].astype('int')
+        process_binary(X)
         fake_label = np.random.randint(0, 2, (X.shape[0], 1))
         X = np.concatenate([X, fake_label], axis=1)
         train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.1, random_state=SEED)
+    min_X = np.min(train_X, axis=0)
+    train_X -= min_X
+    test_X -= min_X
+    max_X = np.max(train_X, axis=0)
+    train_X /= max_X
+    test_X /= max_X
     return train_X, test_X, train_Y, test_Y
