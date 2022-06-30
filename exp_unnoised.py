2b1f9870de351093783713fc7861a53e60d4fd66
叶芃 <isdkfj@qq.com>
2022-06-30 21:36:42 +0800

    Update exp_unnoised.py

--- exp_unnoised.py
+++ exp_unnoised.py
@@ -31,7 +31,7 @@ elif args.data == 'nursery':
 elif args.data == 'covertype':
     num_classes = 7
     dimensions = [11]
-    hid = [300, 100, 10]
+    hid = args.net
 
 def run_exp(d1, num_exp, mask):
     sum_train_acc = 0
