2478b6785788fc41d7b12b4a180c459c626cda49
叶芃 <isdkfj@qq.com>
2022-07-01 15:23:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,25 +48,24 @@ def run_exp(d1, num_exp, mask):
     sum_attack_acc = 0
     for iter_exp in range(num_exp):
         net = Net(d1, train_X.shape[1] - d1 - 1, num_classes, hid, mask.defense)
-        train(net, (train_dataset, train_loader), verbose=True)
+        train(net, (train_dataset, train_loader), verbose=False)
         train_acc, test_acc, attack_acc, idx = eval(net, (train_dataset, train_loader, test_dataset, test_loader), binary_features)
         sum_train_acc += train_acc
         sum_test_acc += test_acc
         sum_attack_acc += attack_acc
-        print(train_acc, test_acc, attack_acc)
     mask.print_info(sum_train_acc / num_exp, sum_test_acc / num_exp, sum_attack_acc / num_exp)
 
 for d1 in dimensions:
     #gauss = Gaussian(0.0)
     #run_exp(d1, 10, gauss)
-    '''gauss = Gaussian(0.01)
+    gauss = Gaussian(0.01)
     run_exp(d1, 10, gauss)
     gauss = Gaussian(0.05)
     run_exp(d1, 10, gauss)
     gauss = Gaussian(0.1)
     run_exp(d1, 10, gauss)
     gauss = Gaussian(0.5)
-    run_exp(d1, 10, gauss)'''
+    run_exp(d1, 10, gauss)
     fab = Defense(d1)
     run_exp(d1, 10, fab)
         
