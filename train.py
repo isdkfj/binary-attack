a562c20c19c4523118f9b0428000db57647f4930
叶芃 <isdkfj@qq.com>
2022-07-02 15:39:35 +0800

    Update train.py

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ def prepare_dataset(train_X, train_Y, test_X, test_Y, batch_size):
 def train(net, data, verbose=False):
     train_dataset, train_loader= data
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 60, 90])
 
     num_epoch = 100
