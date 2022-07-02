55348970d6f2d1c8d101253a69f9d0a2a4d93c6c
叶芃 <isdkfj@qq.com>
2022-07-02 18:56:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -34,6 +34,8 @@ def train(net, data, verbose=False):
 
     for epoch in range(1, num_epoch + 1):
         for i, (data, target) in enumerate(train_loader):
+            if i == 0:
+                print(data)
             optimizer.zero_grad()
             output = net(data)
             loss = criterion(output, target)
