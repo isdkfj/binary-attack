38f5679b077677e8f4ddaa19ee8ec05b3e54ffec
叶芃 <isdkfj@qq.com>
2022-07-02 15:27:02 +0800

    Update train.py

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ def train(net, data, verbose=False):
             nn.utils.clip_grad_value_(net.parameters(), 2)
             optimizer.step()
         scheduler.step()
-        if epoch % 1 == 0 and epoch <= 10 and verbose:
+        if (epoch % 10 == 0 or epoch <= 10) and verbose:
             with torch.no_grad():
                 total_loss = 0.0
                 total_acc = 0.0
