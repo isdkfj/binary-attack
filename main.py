bb8d52aba4c620deae145f00308994a601376814
叶芃 <isdkfj@qq.com>
2022-07-02 16:43:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -57,9 +57,9 @@ def run_exp(d1, num_exp, mask):
     mask.print_info(list_train_acc, list_test_acc, list_attack_acc)
 
 for d1 in dimensions:
-    '''gauss = Gaussian(0.0)
+    gauss = Gaussian(0.0)
     run_exp(d1, args.repeat, gauss)
-    gauss = Gaussian(0.01)
+    '''gauss = Gaussian(0.01)
     run_exp(d1, args.repeat, gauss)
     gauss = Gaussian(0.05)
     run_exp(d1, args.repeat, gauss)
