4e82209a08a311c7006eaf6da00e5afd9b0c65c4
叶芃 <isdkfj@qq.com>
2022-07-04 20:34:52 +0800

    Update exp_unnoised.py

--- exp_unnoised.py
+++ exp_unnoised.py
@@ -14,40 +14,42 @@ train_dataset, train_loader, test_dataset, test_loader = prepare_dataset(train_X
 
 if args.data == 'bank':
     num_classes = 2
-    dimensions = [8]
+    d1 = 8
     hid = [60, 30, 10]
 elif args.data =='credit':
     num_classes = 2
-    dimensions = [10]
-    hid = [50, 20, 10]
+    d1 = 10
+    hid = [100, 50, 20]
 elif args.data == 'mushroom':
     num_classes = 2
-    dimensions = [15]
+    d1 = 15
     hid = [50, 20]
+    # swap binary features out
+    train_X[:, [5, 6, 7, 9, 15, 16, 17, 18]] = train_X[:, [15, 16, 17, 18, 5, 6, 7, 9]]
+    test_X[:, [5, 6, 7, 9, 15, 16, 17, 18]] = test_X[:, [15, 16, 17, 18, 5, 6, 7, 9]]
 elif args.data == 'nursery':
     num_classes = 5
-    dimensions = [6]
-    hid = [20, 10]
+    d1 = 6
+    hid = [600, 300, 100]
 elif args.data == 'covertype':
     num_classes = 7
-    dimensions = [11]
-    hid = [600, 300, 100]
+    d1 = 11
+    hid = [1000, 600, 300, 100]
 
 def run_exp(d1, num_exp, mask):
-    sum_train_acc = 0
-    sum_test_acc = 0
-    sum_attack_acc = 0
+    list_train_acc = []
+    list_test_acc = []
+    list_attack_acc = []
     for iter_exp in range(num_exp):
         net = Net(d1, train_X.shape[1] - d1 - 1, num_classes, hid, mask.defense)
-        train(net, (train_dataset, train_loader), verbose=args.verbose)
-        train_acc, test_acc, attack_acc, idx = eval(net, (train_dataset, train_loader, test_dataset, test_loader))
-        sum_train_acc += train_acc
-        sum_test_acc += test_acc
-        sum_attack_acc += attack_acc
-    mask.print_info(sum_train_acc / num_exp, sum_test_acc / num_exp, sum_attack_acc / num_exp)
+        train(net, (train_dataset, train_loader, validation_dataset, validation_loader), verbose=args.verbose)
+        train_acc, test_acc, attack_acc, idx = eval(net, (validation_dataset, validation_loader, test_dataset, test_loader), binary_features)
+        list_train_acc.append(train_acc)
+        list_test_acc.append(test_acc)
+        list_attack_acc.append(attack_acc)
+    mask.print_info(list_train_acc, list_test_acc, list_attack_acc)
 
-for d1 in dimensions:
-    gauss = Gaussian(0.0)
-    run_exp(d1, args.repeat, gauss)
+gauss = Gaussian(0.0)
+run_exp(d1, args.repeat, gauss)
         
         
