a15e3b97364c3300daac4d50c23a5d1f36049f89
叶芃 <isdkfj@qq.com>
2022-07-15 07:55:48 +0800

    Update defend.py

--- defend.py
+++ defend.py
@@ -7,7 +7,7 @@ class Gaussian:
         self.eps = eps
         self.generator = torch.Generator()
 
-    def defense(self, x1, *args):
+    def defense(self, x1):
         return torch.randn(x1.size(), generator=self.generator) * self.eps
 
     def print_info(self, train_acc, test_acc, attack_acc):
@@ -22,26 +22,9 @@ class Defense:
         self.d1 = d1
         self.binary_features = binary_features
 
-    def defense(self, x1, x, W):
-        invW = torch.linalg.inv(W[:self.d1, :].T)
-        Q = torch.linalg.solve(W[:self.d1, :].T, W.T)
-        w = torch.mean(invW[:, self.binary_features], axis=1)
-        # construct quadratic programming
-        mat = torch.zeros((self.d1 + 1, self.d1 + 1))
-        mat[:self.d1, :self.d1] = 2 * Q @ Q.T
-        mat[:self.d1, -1] = w
-        mat[-1, :self.d1] = w
-        vec = torch.zeros(self.d1 + 1)
-        vec[-1] = 1
-        sol = torch.linalg.solve(mat, vec)
-        r = x[:, -1].reshape(-1, 1) - x1[:, :self.d1] @ w.reshape(-1, 1)
-        r = r @ sol[:self.d1].reshape(1, -1)
-        r = r[:, :self.d1] @ Q
-        return r.detach()
-
     @staticmethod
     def print_info(train_acc, test_acc, attack_acc):
-        print('our method')
+        print('misguided approach')
         print_stat('train_acc', train_acc)
         print_stat('test_acc', test_acc)
         print_stat('attack_acc', attack_acc)
