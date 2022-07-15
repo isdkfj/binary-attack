ff88ba45abf9726d61e52f3360d381f48fb027a8
叶芃 <isdkfj@qq.com>
2022-07-15 15:13:24 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -57,5 +57,5 @@ def print_stat(name, a):
 def powerset(s):
     x = len(s)
     masks = [1 << i for i in range(x)]
-    for i in range(1 << x):
+    for i in range(1, 1 << x):
         yield [ss for mask, ss in zip(masks, s) if i & mask]
