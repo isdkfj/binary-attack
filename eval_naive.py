45b6da6fb149a75c870a1c58b5d41a80dde3b16c
叶芃 <isdkfj@qq.com>
2022-07-16 16:28:40 +0800

    Update eval_naive.py

--- eval_naive.py
+++ eval_naive.py
@@ -57,6 +57,6 @@ def eval(net, data, bf):
         if np.sum(np.isclose(X[:, -1], sol)) == X.shape[0]:
             print('attack fake feature successfully.'.format(i))'''
     for sol in ans:
-        w = np.linalg.solve(X, sol)
+        w = numpy.linalg.lstsq(X, sol)[0]
         print(w)
     return train_acc, test_acc, best_acc, idx
