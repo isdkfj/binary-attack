2d986603c72af707ca0b9d4351afdbc001399293
叶芃 <isdkfj@qq.com>
2022-12-26 14:32:12 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def get_args():
     parser.add_argument('--repeat', type=int, help='number of trials', default=10)
     parser.add_argument('--verbose', action='store_true', help='print train accuracy and loss')
     # attack and defense method
-    parser.add_argument('--am', type=str, help='attack method', choices=['linear', 'regression'], default='regression')
+    parser.add_argument('--am', type=str, help='attack method', choices=['no', 'linear', 'regression'], default='no')
     parser.add_argument('--dm', type=str, help='defense method', choices=['gauss', 'fake'], default='gauss')
     parser.add_argument('--eps', type=float, help='eps in gaussian defense', default=0.0)
     # federated option
