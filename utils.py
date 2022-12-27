c8ee52aa423aeee66569c8c3f7117fe32cd25ccc
叶芃 <isdkfj@qq.com>
2022-12-28 01:37:02 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def get_args():
     # dataset
     parser.add_argument('--path', type=str, help='dataset directory', default='./data')
     parser.add_argument('--data', type=str, help='dataset',
-                        choices=['bank', 'credit', 'mushroom', 'nursery', 'covertype', 'covid'],
+                        choices=['bank', 'credit', 'mushroom', 'nursery', 'covertype', 'covid', 'monkey'],
                         default='bank')
     # neural network hyper-parameters
     parser.add_argument('--net', type=int, nargs='*', help='number of neurons in each hidden layer', default=[600, 300, 100])
