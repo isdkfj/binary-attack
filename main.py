2eef892ed63351013b984530f66968187164ebba
叶芃 <isdkfj@qq.com>
2023-01-03 17:43:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if args.data == 'bank':
     hid = [60, 30, 10]
 elif args.data =='credit':
     num_classes = 2
-    d1 = 10
+    d1 = 8
     hid = [100, 50, 20]
 elif args.data == 'mushroom':
     num_classes = 2
