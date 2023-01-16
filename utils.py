e4c9bb3fe23b4bbe7d41679c6ff1042fa39e6ed0
叶芃 <isdkfj@qq.com>
2023-01-16 21:08:36 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,6 +18,7 @@ def get_args():
     # experiment settings
     parser.add_argument('--repeat', type=int, help='number of trials', default=10)
     parser.add_argument('--verbose', action='store_true', help='print train accuracy and loss')
+    parser.add_argument('--time', action='store_true', help='print time')
     # attack and defense method
     parser.add_argument('--am', type=str, help='attack method', choices=['no', 'linear', 'regression', 'ada'], default='no')
     parser.add_argument('--dm', type=str, help='defense method', choices=['gauss', 'fake'], default='gauss')
